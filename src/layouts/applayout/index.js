4b547082d939a2f4f6d78d74ee7ddcf77f564a64
Le Nhut Hao <haole01627@gmail.com>
2021-04-11 15:57:31 +0700

    add

---
+++ src/layouts/applayout/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '@/components/header/Header';
+function AppLayout() {
+	return (
+		<div>
+			<h1>layout</h1>
+		</div>
+	);
+}
+
+export default AppLayout;
