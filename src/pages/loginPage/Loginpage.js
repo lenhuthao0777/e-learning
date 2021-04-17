b44fcee3d96780134fb46f80e0e866271dd4ef3c
Le Nhut Hao <haole01627@gmail.com>
2021-04-17 21:54:19 +0700

    update

--- src/pages/loginPage/Loginpage.js
+++ src/pages/loginPage/Loginpage.js
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { Input, FormGroup, Label } from 'reactstrap';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 function Loginpage() {
 	// controlled compoent: conttrol tat ca moi thu tren dao dien bang state, props
 	// uncontrooled component: controll dao dien ko thong qua state , props
@@ -13,14 +15,25 @@ function Loginpage() {
 		// console.log(inputMatKhau.current.value);
 		console.log(value);
 	};
+	const schema = yup.object().shape({
+		taiKhoan: yup
+			.string()
+			.required('tai khoan ko dc de trong')
+			.min(5, 'tai khoan tu 5 den 20 ky tu')
+			.max(20, 'tai khoan < 20 ky tu'),
+		matKhau: yup.string().required('mat khau ko dc de trong'),
+	});
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 		// su dung khi component ko ho tro register
 		control,
-	} = useForm();
+	} = useForm({ resolver: yupResolver(schema) });
 	console.log(errors);
+
+	// tao schema validation
+
 	return (
 		<form onSubmit={handleSubmit(handelLogin)}>
 			{/* <h1>Login page</h1>
@@ -49,49 +62,17 @@ function Loginpage() {
 				placeholder='mat khau'
 			/>
 			{errors.matKhau && <div>mat khau ko dc de trong</div>} */}
-			{/* <FormGroup>
+			<FormGroup>
 				<Label>Tai Khoan</Label>
-				<Input
-					type='text'
-					{...register('taiKhoan', {
-						required: {
-							value: true,
-							message: 'tai khoan khong duoc de trong',
-						},
-						minLength: {
-							value: 5,
-							message: 'tai khoan tu 5 den 20 ky tu',
-						},
-						maxLength: {
-							value: 20,
-							message: 'tai khoan < 20 ky tu',
-						},
-					})}
-				/>
+				<Input type='text' {...register('taiKhoan')} />
 				{errors.taiKhoan && <div>{errors.taiKhoan.message}</div>}
 			</FormGroup>
 			<FormGroup>
 				<Label>Mat Khau</Label>
-				<Input
-					type='text'
-					{...register('matKhau', {
-						required: {
-							value: true,
-							message: 'mat khau khong duoc de trong',
-						},
-						minLength: {
-							value: 5,
-							message: 'mat khau tu 5 den 20 ky tu',
-						},
-						maxLength: {
-							value: 20,
-							message: 'mat khau < 20 ky tu',
-						},
-					})}
-				/>
+				<Input type='text' {...register('matKhau')} />
 				{errors.matKhau && <div>{errors.matKhau.message}</div>}
-			</FormGroup> */}
-			<FormGroup>
+			</FormGroup>
+			{/* <FormGroup>
 				<Controller
 					name='taiKhoan'
 					control={control}
@@ -107,7 +88,7 @@ function Loginpage() {
 					}}
 				/>
 				{errors.taiKhoan && <div>{errors.taiKhoan.message}</div>}
-			</FormGroup>
+			</FormGroup> */}
 			<button>Login</button>
 		</form>
 	);
